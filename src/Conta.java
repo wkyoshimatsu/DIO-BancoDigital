2aa4e20a86dfc2971341633f0fc7eab2e9d51e95
wkyoshimatsu <101765683+wkyoshimatsu@users.noreply.github.com>
2024-01-12 12:57:12 -0300

    Alterado a versão do JDK de 20 para 21. Melhorias nas classes tipo Conta

--- src/Conta.java
+++ src/Conta.java
@@ -1,20 +1,48 @@
-public interface Conta {
-    private char tipoConta;
-    private Cliente cliente;
-    private int numeroAgencia;
-    private long numeroConta;
-    private int digitoConta;
-    private double saldo;
+public abstract class Conta implements IConta{
 
-    public void sacar{
+    private static final int AGENCIA_PADRAO = 0001;
+    private static int SEQUENCIAL = 1;
+
+    protected int agencia;
+    protected long numero;
+    protected double saldo;
+
+    @Override
+    public void sacar(double valor) {
+        this.saldo -= valor;
+        System.out.println("=================================");
+        System.out.println("Saque realizado com sucesso");
+        System.out.println("Seu saldo atual é: " + this.saldo);
+        System.out.println("=================================");
+
 
     }
 
-    public void depositar{
+    @Override
+    public void depositar(double valor) {
 
     }
 
-    public void transferir{
+    @Override
+    public void transferir(double valor, Conta contaDestino) {
 
+    }
+
+    public Conta(){
+        this.agencia = AGENCIA_PADRAO;
+        this.numero = SEQUENCIAL;
+    }
+
+
+    public int getAgencia() {
+        return agencia;
+    }
+
+    public long getNumero() {
+        return numero;
+    }
+
+    public double getSaldo() {
+        return saldo;
     }
 }
--- src/ContaCorrente.java
+++ src/ContaCorrente.java
@@ -1,36 +1,3 @@
-public class ContaCorrente {
-    //private char tipoConta;
-    //private Cliente cliente;
-    private int numeroAgencia;
-    private long numeroConta;
-    private int digitoConta;
-    private double saldo;
+public class ContaCorrente extends Conta{
 
-    public void sacar{
-
-    }
-
-    public void depositar{
-
-    }
-
-    public void transferir{
-
-    }
-
-    public int getNumeroAgencia() {
-        return numeroAgencia;
-    }
-
-    public long getNumeroConta() {
-        return numeroConta;
-    }
-
-    public int getDigitoConta() {
-        return digitoConta;
-    }
-
-    public double getSaldo() {
-        return saldo;
-    }
 }
