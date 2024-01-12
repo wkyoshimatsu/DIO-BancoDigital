a3562a390de6adae86af9b832eaa45946fa60c6a
wkyoshimatsu <101765683+wkyoshimatsu@users.noreply.github.com>
2024-01-12 13:50:08 -0300

    Implementado métodos da classe Conta.java. - sacar - depositar - transferir

--- src/Conta.java
+++ src/Conta.java
@@ -1,7 +1,7 @@
 public abstract class Conta implements IConta{
 
     private static final int AGENCIA_PADRAO = 0001;
-    private static int SEQUENCIAL = 1;
+    private static long SEQUENCIAL = 00001;
 
     protected int agencia;
     protected long numero;
@@ -10,22 +10,32 @@ public abstract class Conta implements IConta{
     @Override
     public void sacar(double valor) {
         this.saldo -= valor;
-        System.out.println("=================================");
-        System.out.println("Saque realizado com sucesso");
+        System.out.println("====================================================");
+        System.out.println("Saque de " + valor + "realizado com sucesso");
         System.out.println("Seu saldo atual é: " + this.saldo);
-        System.out.println("=================================");
+        System.out.println("====================================================");
 
 
     }
 
     @Override
     public void depositar(double valor) {
+        this.saldo += valor;
+        System.out.println("====================================================");
+        System.out.println("Depósito de " + valor + " realizado com sucesso");
+        System.out.println("Seu saldo atual é: " + this.saldo);
+        System.out.println("====================================================");
 
     }
 
     @Override
     public void transferir(double valor, Conta contaDestino) {
-
+        this.sacar(valor);
+        contaDestino.depositar(valor);
+        System.out.println("====================================================");
+        System.out.println("Transferência de " + valor + " realizada com sucesso");
+        System.out.println("Seu saldo atual é: " + this.saldo);
+        System.out.println("====================================================");
     }
 
     public Conta(){
