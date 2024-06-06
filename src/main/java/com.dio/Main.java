8a1fc47a66d17f2f6d55653c289a1b97e26e8c18
wkyoshimatsu <101765683+wkyoshimatsu@users.noreply.github.com>
2024-06-06 10:18:54 -0300

    Arrumando TerminalBanco.java

--- src/main/java/com.dio/Main.java
+++ src/main/java/com.dio/Main.java
@@ -2,16 +2,21 @@ package com.dio;
 
 public class Main {
     public static void main(String[] args) {
+        System.out.println("Teste");
         TerminalBanco terminal = new TerminalBanco();
         int opcao;
         do{
             opcao = terminal.exibirMenuPrincipal();
+
+            do if (opcao == 1) {
+                opcao = terminal.exibirCadastrarConta();
+            } else if (opcao == 2) {
+                terminal.exibirAcessarConta();
+            } else {
+                System.out.println("erro");
+            } while(opcao != 0);
+
         } while(opcao == -1);
 
-        do if (opcao == 1) {
-            opcao = terminal.exibirCadastrarConta();
-        } else {
-            terminal.exibirAcessarConta();
-        } while(opcao != 0);
     }
 }
--- src/main/java/com.dio/TerminalBanco.java
+++ src/main/java/com.dio/TerminalBanco.java
@@ -3,24 +3,36 @@ package com.dio;
 import java.util.*;
 
 public class TerminalBanco {
-    private final int [] OPCOES_VALIDAS_PRINCIPAL = {1, 2};
+    private final int [] OPCOES_VALIDAS_MENU_PRINCIPAL = {1, 2, 3};
     private final int [] OPCOES_VALIDAS_CADASTRO_CONTA = {1, 2};
+
     public int exibirMenuPrincipal(){
 
         System.out.println("Bem vindo ao Banco");
         System.out.println("Selecione uma das opções abaixo");
         System.out.println("1 - Cadastrar Conta");
         System.out.println("2 - Acessar Conta");
+        System.out.println("3 - Sair");
         System.out.println("Digite a opção desejada:");
 
-        try (Scanner scanner = new Scanner(System.in)) {
-            int opcao = scanner.nextInt();
-            checarOpcao(opcao, OPCOES_VALIDAS_PRINCIPAL);
+        Scanner scanner = new Scanner(System.in);
+        int opcao = scanner.nextInt();
+        boolean eValido = false;
+
+        for (int opcaoValida : OPCOES_VALIDAS_MENU_PRINCIPAL) {
+            if (opcao == opcaoValida) {
+                eValido = true;
+                break;
+            }
+        }
+
+        if (eValido) {
             return opcao;
-        } catch (Exception e) {
-            System.out.println(e.getMessage());
+        } else {
+            System.out.println("Opção inválida");
             return -1;
         }
+
     }
 
     public int exibirCadastrarConta(){
@@ -36,33 +48,36 @@ public class TerminalBanco {
 
         System.out.println("Selecione o tipo de conta que deseja criar");
         System.out.println("1 - Conta Corrente");
-        System.out.println("1 - Conta Poupança");
+        System.out.println("2 - Conta Poupança");
         System.out.println("Digite a opção desejada:");
 
-        try {
-            int opcao = scanner.nextInt();
-            checarOpcao(opcao, OPCOES_VALIDAS_CADASTRO_CONTA);
+        int opcao = scanner.nextInt();
+        boolean eValido = false;
 
-            if (opcao == 1){
-                ContaCorrente contaCorrente = new ContaCorrente(novoCliente);
-                contaCorrente.exibirDados();
-            } else {
-                ContaPoupanca contaPoupanca = new ContaPoupanca(novoCliente);
-                contaPoupanca.exibirDados();
+        for (int opcaoValida : OPCOES_VALIDAS_CADASTRO_CONTA) {
+            if (opcao == opcaoValida) {
+                eValido = true;
+                break;
             }
-
-            System.out.println("====================================================");
-            System.out.println("Conta criada com sucesso!");
-            System.out.println("====================================================");
-            return 0; // para o menu principal voltar para o menu principal sem executar o cadastro de conta.
-
-        } catch (Exception e) {
-            System.out.println(e.getMessage());
-            return -1;
-
-        } finally {
-            scanner.close();
         }
+
+        if (!eValido) {
+            System.out.println("Opção inválida");
+            return -1;
+        }
+
+        if (opcao == 1){
+            ContaCorrente contaCorrente = new ContaCorrente(novoCliente);
+            contaCorrente.exibirDados();
+        } else if (opcao == 2){
+            ContaPoupanca contaPoupanca = new ContaPoupanca(novoCliente);
+            contaPoupanca.exibirDados();
+        }
+
+        System.out.println("====================================================");
+        System.out.println("Conta criada com sucesso!");
+        System.out.println("====================================================");
+        return 0;
     }
 
     public void exibirAcessarConta(){
